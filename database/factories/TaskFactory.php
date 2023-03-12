c22587318b767c3d9d66db623f75a74a8fa05de7
Jonathan Emanuels <j.emanuels@outlook.com>
2023-03-12 23:04:15 +0100

    #7
    - Add Taskfactory
    - Edit the unittest

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->sentence(),
+            'description' => fake()->paragraph(),
+            'task_list_id' => fake()->uuid(),
+            'user_id' => fake()->uuid(),
+        ];
+    }
+}
--- tests/Unit/UserServicesTest.php
+++ tests/Unit/UserServicesTest.php
@@ -2,17 +2,47 @@
 
 namespace Tests\Unit;
 
+use App\Models\User;
 use App\Services\UserServices;
+use App\Services\UserVerificationService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class UserServicesTest extends TestCase
 {
+    use RefreshDatabase;
+
+    private const EMAIL =  'test@mail.nl';
+
     public function testCreateUserSuccessful()
     {
-        $stub = $this->createStub(UserServices::class);
+        $userVerificationService = new UserVerificationService();
+        $userServices = new UserServices($userVerificationService);
 
-        $stub->method('checkIfEmailIsAlreadyInUse')
-            ->willReturn(true);
+        $user = $userServices->createUser([
+            'name' => 'test',
+            'password' => 'test',
+            'email' => self::EMAIL
+        ]);
+
+        $this->assertEquals(self::EMAIL, $user->email);
+    }
+
+
+    public function testCreateUserNotSuccessfulDueToDuplicate()
+    {
+        $userVerificationService = new UserVerificationService();
+        $userServices = new UserServices($userVerificationService);
+
+        $expectedUser = User::factory()->create();
+
+        $user = $userServices->createUser([
+            'name' => 'test',
+            'password' => 'test',
+            'email' => $expectedUser->email
+        ]);
+
+        $this->assertNull($user);
     }
 
 }
